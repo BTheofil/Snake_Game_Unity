1ca80df5222f251a656082a54109ebe38a269270
Theo <btheofil7@gmail.com>
2020-07-27 03:12:08 +0200

    Setups

---
+++ TheSnakeGameProject/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    private void Start()
+    {
+        GameObject snakeHeadGameObject = new GameObject();
+        SpriteRenderer snakeSpriteRenderer = snakeHeadGameObject.AddComponent<SpriteRenderer>();
+        snakeSpriteRenderer.sprite = GameAssets.instance.snakeHeadSprite;
+    }
+}
